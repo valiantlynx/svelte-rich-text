8e63179dda908b55d57449f266a391991c2e566b
valiantlynx <gormerykombo@gmail.com>
2024-03-21 20:46:10 +0100

    svelte-rich-text-terrafoerm a record

--- terraform/modules/cloudflare-ddns/main.tf
+++ terraform/modules/cloudflare-ddns/main.tf
@@ -1,19 +1,9 @@
 resource "cloudflare_record" "ec2_dns_a" {
   for_each = var.cloudflare_zone_ids
   zone_id  = each.value
-  name     = each.key
+  name     = "svelte-rich-text.${each.key}"
   value    = var.public_ip[0]
   type     = "A"
-  ttl      = 1 // Auto
-  proxied  = false
-}
-
-resource "cloudflare_record" "ec2_dns_cname" {
-  for_each = var.cloudflare_zone_ids
-  zone_id  = each.value
-  name     = "www.${each.key}"
-  value    = each.key
-  type     = "CNAME"
   ttl      = 1 // Auto
   proxied  = false
 }
